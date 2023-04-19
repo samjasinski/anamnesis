d1e7b79bac6f850938c8f66d858c0b26c5c79390
Jasinski, Sam (AAHL, Geelong ACDP) <Sam.Jasinski@csiro.au>
2023-04-20 09:44:43 +1000

    trying to fix master..

---
+++ build/precache-manifest.8afeee8a2eed001022850454d9f42f62.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5b8258666b6e8f4fd8d",
+    "url": "/anamnesis/static/js/main.e5b82586.chunk.js"
+  },
+  {
+    "revision": "fcb97ed7d8b060fef237",
+    "url": "/anamnesis/static/js/1.fcb97ed7.chunk.js"
+  },
+  {
+    "revision": "3ab6965f6eec2d7ec6a8",
+    "url": "/anamnesis/static/js/runtime~main.3ab6965f.js"
+  },
+  {
+    "revision": "0971d9d6e7ab4d0aa2fcdd8fe9bb3923",
+    "url": "/anamnesis/index.html"
+  }
+];
